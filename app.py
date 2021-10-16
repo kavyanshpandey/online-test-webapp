0c5188e09200f5d5d8c937dfe29a1557a6f28df4
NY <nidhisha@debian>
2021-10-16 23:52:39 +0530

    Typo fixes added,

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def exam():
     name = input("Please enter your name to start the test", type ="text", validate = validate_name)
 
 
-    q1 = radio("Q1. Base language of web?",['javaScript','ASP','PHP','HTML'])
+    q1 = radio("Q1. Base language of web?",['JavaScript','ASP','PHP','HTML'])
     if q1 =='HTML':
         c+=1
 
@@ -25,15 +25,15 @@ def exam():
     if q2 =='HTML':
         c+=1
 
-    q3 = radio("Q3. Secondery memory is also called _____",['Virtual memory','RAM','ROM','Hard drives'])
+    q3 = radio("Q3. Secondary memory is also called _____",['Virtual memory','RAM','ROM','Hard drives'])
     if q3 =='ROM':
         c+=1
 
-    q4 = radio("Q4. functions that is used to get th length of string in Python",['count()','length()','dis()','len()'])
+    q4 = radio("Q4. Function used to get the length of string in Python",['count()','length()','dis()','len()'])
     if q4 == 'len()':
         c+=1
 
-    q5 = radio("Q5. Which is not a web framework",['Django','React','Numpy','Angular'])
+    q5 = radio("Q5. Which is not a web framework?",['Django','React','Numpy','Angular'])
     if q5 == 'Numpy':
         c+=1
 
