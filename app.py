7f9a38d1f5a45146a233730474d3a09126d85fd3
Vidya S <vidyashivakumar221@gmail.com>
2021-10-28 20:17:38 +0530

    Corrected Questions

--- app.py
+++ app.py
@@ -17,19 +17,19 @@ def exam():
     name = input("Please enter your name to start the test", type ="text", validate = validate_name)
 
 
-    q1 = radio("Q1. Base language of web?",['javaScript','ASP','PHP','HTML'])
+    q1 = radio("Q1. Base language of web?",['javaScript','ASP','PHP','HTML','.NET'])
     if q1 =='HTML':
         c+=1
 
-    q2 = radio("Q2. Which is not a programming language",['Python','HTML','Scala','Java'])
+    q2 = radio("Q2. Which is not a programming language",['Python','HTML','Scala','Java','C#'])
     if q2 =='HTML':
         c+=1
 
-    q3 = radio("Q3. Secondery memory is also called _____",['Virtual memory','RAM','ROM','Hard drives'])
+    q3 = radio("Q3. Secondary memory is also called _____",['Virtual memory','RAM','ROM','Hard drives'])
     if q3 =='ROM':
         c+=1
 
-    q4 = radio("Q4. functions that is used to get th length of string in Python",['count()','length()','dis()','len()'])
+    q4 = radio("Q4. functions that is used to get the length of string in Python",['count()','length()','dis()','len()'])
     if q4 == 'len()':
         c+=1
 
