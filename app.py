81bff04f52d87733d57db9f97086db9ce1510d3e
Anjali Deep <45489595+anjali142@users.noreply.github.com>
2021-10-30 15:15:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def exam():
     if q1 =='HTML':
         c+=1
 
-    q2 = radio("Q2. Which is not a programming language",['Python','HTML','Scala','Java'])
+    q2 = radio("Q2. Which is not a programming language?",['Python','HTML','Scala','Java'])
     if q2 =='HTML':
         c+=1
 
@@ -29,22 +29,22 @@ def exam():
     if q3 =='ROM':
         c+=1
 
-    q4 = radio("Q4. functions that is used to get th length of string in Python",['count()','length()','dis()','len()'])
+    q4 = radio("Q4. Functions that is used to get th length of string in Python",['count()','length()','dis()','len()'])
     if q4 == 'len()':
         c+=1
 
-    q5 = radio("Q5. Which is not a web framework",['Django','React','Numpy','Angular'])
+    q5 = radio("Q5. Which is not a web framework?",['Django','React','Numpy','Angular'])
     if q5 == 'Numpy':
         c+=1
 
     if c>3:
-    	message = [style(put_html("<h1 style='display:inline;border-bottom:0px'>Congratulations !! </h1>"+ name + ", your score is <b>"+ str(c) + "</b><br><br>") ,'color:green;'),style(put_html("<p>Result : <b>PASSED</b></p>"),'color:green'), put_html("<b>Thank You for your participation.</b>")]
+    	message = [style(put_html("<h1 style='display:inline;border-bottom:0px'>Congratulations !! </h1>"+ name + ", your score is <b>"+ str(c) + "</b><br><br>") ,'color:green;'),style(put_html("<p>Result : <b>PASSED!</b></p>"),'color:green'), put_html("<b>Thank You for your participation!.</b>")]
     	popup("Result", content=message, size='large', implicit_close=True, closable=True)
     else:
-    	message = [style(put_html("<h1 style='display:inline;border-bottom:0px'>Oops! " + "</h1>" + name + ", your score is <b>"+ str(c) + "</b><br><br>"),'color:red'), style(put_html("<p>Result : <b>FAILED</b></p>"), 'color:red') , put_html("<b>Thank You for your participation.</b><br><br>"), style(put_link('Retry ↺',""), 'color:red;align-content: center;border-radius: 5px;color:#f9faf8;padding: 5px 100px;text-align:center;align-items : center;background-color: white;\
+    	message = [style(put_html("<h1 style='display:inline;border-bottom:0px'>Oops! " + "</h1>" + name + ", your score is <b>"+ str(c) + "</b><br><br>"),'color:red'), style(put_html("<p>Result : <b>FAILED!</b></p>"), 'color:red') , put_html("<b>Thank You for your participation!.</b><br><br>"), style(put_link('Retry ↺',""), 'color:red;align-content: center;border-radius: 5px;color:#f9faf8;padding: 5px 100px;text-align:center;align-items : center;background-color: white;\
             background-image: linear-gradient(270deg, #8cf5f5 1%, #0a43f3 100%);')]
     	popup("Result", content=message, size='large', implicit_close=True, closable=True)
-"""A method to validate the name entered by user"""
+"""A method to validate the name entered by the user"""
 def validate_name(name):
 	#removing all spaces from the input name
 	name = name.replace(" ","")
